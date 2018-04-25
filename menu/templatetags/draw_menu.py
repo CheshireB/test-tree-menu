e80f18c91907930a322024739c8bd5e598a19aa7
Cheshire.B <NIkitaUniversity@yandex.ru>
2018-04-26 00:51:08 +0300

    small refactoring: draw_menu on func

--- menu/templatetags/draw_menu.py
+++ menu/templatetags/draw_menu.py
@@ -9,26 +9,16 @@ register = template.Library()
 def draw_menu(context, menu_name):
 
 
-
     try:
         item_queryset = Item.objects.filter(menu_id=menu_name)
         item_queryset_values = item_queryset.values()
-        root_item = [item for item in item_queryset_values.filter(parent_item=None)]
 
+        root_item = [item for item in item_queryset_values.filter(parent_item=None)]
         selected_item_id = int(context['request'].GET[menu_name])
         selected_item = item_queryset.get(id=selected_item_id)
 
         parent_item = selected_item
-        selected_item_id_list = []
-
-        while parent_item:
-            selected_item_id_list.append(parent_item.id)
-            parent_item = parent_item.parent_item
-
-        if not selected_item_id_list:
-            for item in root_item:
-                if item['id'] == selected_item_id:
-                    selected_item_id_list.append(selected_item_id)
+        selected_item_id_list = get_selected_item_id_list(parent_item, root_item, selected_item_id)
 
         for item in root_item:
             if item['id'] in selected_item_id_list:
@@ -36,26 +26,48 @@ def draw_menu(context, menu_name):
 
         result_dict = {'items': root_item,}
 
-
     except:
         selected_item_id = None
         result_dict = {'items':[item for item in Item.objects.filter(menu_id=menu_name, parent_item=None).values()]}
 
     result_dict['menu_name'] = menu_name
+    result_dict['other_querystring'] = get_querystring(context, menu_name)
 
+    return result_dict
+
+
+def get_querystring(context, menu_name):
     querystring_args = []
+
     for key in context['request'].GET:
         if key != menu_name:
-            querystring_args.append(key+'='+context['request'].GET[key])
+            querystring_args.append(key + '=' + context['request'].GET[key])
+
     querystring = ('&').join(querystring_args)
 
-    result_dict['put_url'] = querystring
-    return result_dict
+    return querystring
 
 
 def get_child_items(item_queryset_values, current_item_id, selected_item_id_list):
     item_list = [item for item in item_queryset_values.filter(parent_item_id=current_item_id)]
+
     for item in item_list:
         if item['id'] in selected_item_id_list:
             item['child_items'] = get_child_items(item_queryset_values, item['id'], selected_item_id_list)
+
     return item_list
+
+
+def get_selected_item_id_list(parent_item, root_item, selected_item_id):
+    selected_item_id_list = []
+
+    while parent_item:
+        selected_item_id_list.append(parent_item.id)
+        parent_item = parent_item.parent_item
+
+    if not selected_item_id_list:
+        for item in root_item:
+            if item['id'] == selected_item_id:
+                selected_item_id_list.append(selected_item_id)
+
+    return selected_item_id_list
