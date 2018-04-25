5e250b6cf6e8dc1292c066758dbde841e4d62773
Cheshire.B <NIkitaUniversity@yandex.ru>
2018-04-25 19:40:59 +0300

    created database and registered admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tree, Node
+
+
+class TreeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug']
+
+
+admin.site.register(Tree, TreeAdmin)
+admin.site.register(Node, NodeAdmin)
+
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Tree(models.Model):
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=30, unique=True, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Node(models.Model):
+
+    name = models.CharField(max_length=30)
+    slug = models.CharField(max_length=30)
+
+    parent_node = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+
+    tree = models.ForeignKey(Tree, on_delete=models.CASCADE, default='first_tree')
+
+    def __str__(self):
+        return self.name
